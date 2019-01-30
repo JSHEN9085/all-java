5b499e2c9703957c3a33ace1ad7329d33fe843a7
Jun Jian Shen <jshen9085@gmail.com>
2019-01-30 15:39:53 -0500

    method overloading

---
+++ Overloading/src/com/company/Main.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int newScore = calculateScore("Tim", 500);
+        System.out.println(newScore);
+        calculateScore(750);
+        calculateScore();
+    }
+
+    public static int calculateScore(String playerName, int score){
+        System.out.println("Player " + playerName + " scored " + score + " point ");
+        return score * 1000;
+    }
+
+    public static int calculateScore(int score){
+        System.out.println("Unnamed Player" + " scored " + score + " point ");
+        return score * 1000;
+    }
+
+    public static int calculateScore(){
+        System.out.println("No Player name, no score");
+        return 0;
+    }
+
+//    public static void calculateScore(){
+//        System.out.println("No Player name, no score");
+//
+//    }
+// the signature is their arguments, changing the returned data type
+// won't let Java identify them as two different methods; 
+}
