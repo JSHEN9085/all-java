6facae7424e4cdda4a9bcf4305000a60ba1ab6b8
Jun Jian Shen <jshen9085@gmail.com>
2019-02-02 11:16:58 -0500

    hasNextInt()

--- Scanner/src/com/company/Main.java
+++ Scanner/src/com/company/Main.java
@@ -6,15 +6,26 @@ public class Main {
     public static void main(String[] args) {
 	    Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your year of birth: ");
-        int yearOfBirth = scanner.nextInt();
-        scanner.nextLine(); //without this line, program would direct keep running and we don't have chance to enter the name;
 
-        System.out.println("Enter your name: ");
-        String name = scanner.nextLine();
-        int age = 2018 - yearOfBirth;
+        boolean hasNextInt = scanner.hasNextInt();
+        // hasNextInt() This method returns true if and only if this scanner's next token is a valid int value
 
-        System.out.println("Your name is " + name + ", and you are " + age + " years old.");
+        if (hasNextInt){
+            int yearOfBirth = scanner.nextInt();
+            scanner.nextLine(); //without this line, program would direct keep running and we don't have chance to enter the name;
 
+            System.out.println("Enter your name: ");
+            String name = scanner.nextLine();
+            int age = 2018 - yearOfBirth;
+
+            if (age >= 0 && age <= 150){ //in case user input invalid number in line 9;
+                System.out.println("Your name is " + name + ", and you are " + age + " years old.");
+            } else {
+                System.out.println("Invalid Date of Birth");
+            }
+        } else {
+            System.out.println("Invalid Date of Birth");
+        }
         scanner.close();
     }
 }
