752662c22cec57fa0fb18fc287b03c3bd8b655e2
Jun Jian Shen <jshen9085@gmail.com>
2019-02-23 11:13:41 -0500

    multiple catch

--- Exception/src/com/company/Example.java
+++ Exception/src/com/company/Example.java
@@ -5,8 +5,14 @@ import java.util.Scanner;
 
 public class Example {
     public static void main(String[] args) {
-        int result = divide();
-        System.out.println(result);
+        try {
+            int result = divide();
+            System.out.println(result);
+        } catch (ArithmeticException | NoSuchElementException e){ //press command + D to get here
+            System.out.println(e.toString());
+            System.out.println("unable to perform division");
+        }
+
     }
 
     private static int divide() {
@@ -14,15 +20,13 @@ public class Example {
         try{
             x = getInt();
             y = getInt();
+            System.out.println("x is " + x + ", y is " + y);
+            return x / y;
         } catch (NoSuchElementException e){
-            throw new ArithmeticException("no suitable input");
+            throw new NoSuchElementException("no suitable input");
+        } catch (ArithmeticException e){
+            throw new ArithmeticException("attempt to divide by zero");
         }
-        System.out.println("x is " + x + ", y is " + y);
-        try{
-           return x / y;
-       } catch(ArithmeticException e ){
-           throw new ArithmeticException("attempt to divide by zero");
-       }
     }
 
     private static int getInt(){
