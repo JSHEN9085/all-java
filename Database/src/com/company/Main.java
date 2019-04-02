d4e6bc698be3800449bc1db07e405e8f148fc4f7
Jun Jian Shen <jshen9085@gmail.com>
2019-04-01 23:09:48 -0400

    connected database

--- Database/src/com/company/Main.java
+++ Database/src/com/company/Main.java
@@ -3,15 +3,26 @@ package com.company;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
 
     public static void main(String[] args) {
-        try {
-            Connection connection = DriverManager.getConnection("jdbc:sqlite:/Users/flatironschool/Development/JavaDB/testDB/testjava.db");
-            System.out.println("created db");
-        } catch(SQLException e){
+        try
+        {
+            Connection connection = DriverManager.getConnection("jdbc:sqlite:test.db"); //"jdbc:sqlite:filename.db"; it will create database file in this project file
+            System.out.println("connect db");
+
+            Statement statement = connection.createStatement();
+            statement.execute("CREATE TABLE contacts (name TEXT, phone INTEGER, email TEXT)");
+
+            statement.close();
+            connection.close();
+        }
+        catch(SQLException e)
+        {
             System.out.println("connection error " + e.getMessage());
         }
+
     }
 }
