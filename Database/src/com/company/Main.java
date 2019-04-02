cafb12ad43f24ad02a96f154d193ce51ca459598
Jun Jian Shen <jshen9085@gmail.com>
2019-04-02 00:54:43 -0400

    CRUD refactor

--- Database/src/com/company/Main.java
+++ Database/src/com/company/Main.java
@@ -3,40 +3,48 @@ package com.company;
 import java.sql.*;
 
 public class Main {
+    public static final String DB_NAME = "test.db";
+    public static final String CONNECTION_STRING = "jdbc:sqlite:" + DB_NAME;
+
+    public static final String TABLE_CONTACTS = "contacts";
+
+    public static final String COLUMN_NAME = "name";
+    public static final String COLUMN_PHONE = "phone";
+    public static final String COLUMN_EMAIL = "email";
+
 
     public static void main(String[] args) {
         try
         {
-            Connection connection = DriverManager.getConnection("jdbc:sqlite:test.db"); //"jdbc:sqlite:filename.db"; it will create database file in this project file
+//            Connection connection = DriverManager.getConnection("jdbc:sqlite:test.db"); //hard coding version"jdbc:sqlite:filename.db"; it will create database file in this project file
+            Connection connection = DriverManager.getConnection(CONNECTION_STRING);
             System.out.println("connect db");
 //            connection.setAutoCommit(false); //setting this JDBC won't auto commit, and database won't save any change;
-
             Statement statement = connection.createStatement();
-//            statement.execute("CREATE TABLE IF NOT EXISTS contacts" +
-//                    " (name TEXT, phone INTEGER, email TEXT)");
-//
-//            statement.execute("INSERT INTO contacts (name, phone, email)" +
-//                    " VALUES('Tim', 6545678, 'tim@email.com') ");
-//
-//            statement.execute("INSERT INTO contacts (name, phone, email)" +
-//                    " VALUES('Joy', 1234567, 'joy@email.com') ");
-//
-//            statement.execute("INSERT INTO contacts (name, phone, email)" +
-//                    " VALUES('Jun', 9172222, 'jun@email.com') ");
-//
-//            statement.execute("INSERT INTO contacts (name, phone, email)" +
-//                    " VALUES('Cat', 9876543, 'cat@email.com') ");
 
+            statement.execute("DROP TABLE IF EXISTS " + TABLE_CONTACTS);
+            statement.execute("CREATE TABLE IF NOT EXISTS " + TABLE_CONTACTS +
+                    " (" + COLUMN_NAME +  " TEXT, " +
+                    COLUMN_PHONE +  " INTEGER, " +
+                    COLUMN_EMAIL + " TEXT)");
+
+            insertContact(statement, "Tim", 6545678, "tim@email.com");
+            insertContact(statement, "Joy", 1234567, "joy@email.com");
+            insertContact(statement, "Jun", 9172222, "jun@email.com");
+            insertContact(statement, "Cat", 9876543, "cat@email.com");
+
+//
 //            statement.execute("UPDATE contacts SET phone=1234567 WHERE name='Cat'");
-
+//
 //            statement.execute("DELETE FROM contacts WHERE name='Cat'");
 
-            statement.execute("SELECT * FROM contacts");
-            ResultSet results = statement.getResultSet();
+//            statement.execute("SELECT * FROM contacts");
+//            ResultSet results = statement.getResultSet();
+            ResultSet results = statement.executeQuery("SELECT * FROM " + TABLE_CONTACTS); //same as above two lines;
             while(results.next()){
-                System.out.println(results.getString("name") + " " +
-                                   results.getString("phone") + " " +
-                                   results.getString("email") + "!"
+                System.out.println(results.getString(COLUMN_NAME) + " " +
+                                   results.getString(COLUMN_PHONE) + " " +
+                                   results.getString(COLUMN_EMAIL) + "!"
                 );
             }
 
@@ -49,5 +57,13 @@ public class Main {
             System.out.println("connection error " + e.getMessage());
         }
 
+    }
+
+    private static void insertContact(Statement statement, String name, int phone, String email) throws SQLException {
+        statement.execute("INSERT INTO " + TABLE_CONTACTS +
+                "(" + COLUMN_NAME + "," +
+                COLUMN_PHONE + "," +
+                COLUMN_EMAIL + ") " +
+                "VALUES('" + name + "', " + phone + ", '" + email + "')");
     }
 }
